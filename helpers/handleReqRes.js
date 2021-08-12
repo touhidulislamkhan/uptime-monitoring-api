faf4f0cee5e1746b5ae7d67cc2a3ddbfec165ca9
touhidulislamkhan <touhidikhan0@gmail.com>
2021-08-12 23:09:07 +0600

    handling the request and response

---
+++ helpers/handleReqRes.js
@@ -0,0 +1,62 @@
+// dependencies
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
+const routes = require('../routes');
+const { notFoundHandler } = require('../handlers/routeHandlers/notFoundHandler');
+
+// module scaffolding
+const handler = {};
+
+//handling request response
+handler.handleReqRes = (req, res) => {
+    //handling the request
+    //parsing url
+    const parsedUrl = url.parse(req.url, true);
+    // const parsedUrl = new URL(req.url); //new whatwg url api
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    const requestProperties = {
+        parsedUrl,
+        path,
+        trimmedPath,
+        method,
+        queryStringObject,
+        headersObject,
+    };
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
+
+    chosenHandler(requestProperties, (statusCode, payload) => {
+        statusCode = typeof (statusCode) === 'number' ? statusCode : 500;
+        payload = typeof (payload) === 'object' ? payload : {};
+
+        const payloadString = JSON.stringify(payload);
+
+        res.writeHead(statusCode);
+        res.end(payloadString);
+    });
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    })
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        res.end('hello world');
+    })
+
+    // console.log(parsedUrl);
+
+    //handle response
+}
+
+
+module.exports = handler;
