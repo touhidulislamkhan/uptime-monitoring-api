d83353fb1ae78aab866d21e7d7c8332251b4d480
touhidulislamkhan <touhidikhan0@gmail.com>
2021-08-17 04:31:33 +0600

    put request handling

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -79,6 +79,7 @@ handler._user.post = (requestProperties, callback) => {
 };
 
 handler._user.get = (requestProperties, callback) => {
+   // check if phone number is valid
    const phone =
       typeof requestProperties.queryStringObject.phone === "string" &&
       requestProperties.queryStringObject.phone.trim().length === 11
@@ -103,7 +104,76 @@ handler._user.get = (requestProperties, callback) => {
    }
 };
 
-handler._user.put = (requestProperties, callback) => {};
+handler._user.put = (requestProperties, callback) => {
+   // check if phone number is valid
+   const phone =
+      typeof requestProperties.body.phone === "string" &&
+      requestProperties.body.phone.trim().length === 11
+         ? requestProperties.body.phone
+         : null;
+
+   const firstName =
+      typeof requestProperties.body.firstName === "string" &&
+      requestProperties.body.firstName.trim().length > 0
+         ? requestProperties.body.firstName
+         : null;
+   const lastName =
+      typeof requestProperties.body.lastName === "string" &&
+      requestProperties.body.lastName.trim().length > 0
+         ? requestProperties.body.lastName
+         : null;
+
+   const password =
+      typeof requestProperties.body.password === "string" &&
+      requestProperties.body.password.trim().length > 0
+         ? requestProperties.body.password
+         : null;
+
+   if (phone) {
+      if (firstName || lastName || password) {
+         // lookup the user
+         lib.read("users", phone, (err, data) => {
+            if (!err && data) {
+               const userData = { ...parseJson(data) };
+               if (firstName) {
+                  userData.firstName = firstName;
+               }
+               if (lastName) {
+                  userData.lastName = lastName;
+               }
+               if (password) {
+                  userData.password = hash(password);
+               }
+
+               //update to database
+               lib.update("users", phone, userData, (err2) => {
+                  if (!err2) {
+                     callback(200, {
+                        message: "Information Updated Successfulle!",
+                     });
+                  } else {
+                     callback(500, {
+                        error: "There was a serve side error",
+                     });
+                  }
+               });
+            } else {
+               callback(400, {
+                  error: "You have a problem in your request!",
+               });
+            }
+         });
+      } else {
+         callback(400, {
+            error: "You have a problem in your request!",
+         });
+      }
+   } else {
+      callback(400, {
+         error: "Invalid Phone Number. Please try again!",
+      });
+   }
+};
 
 handler._user.delete = (requestProperties, callback) => {};
 
