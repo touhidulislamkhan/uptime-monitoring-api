04a2caf743fe2501d97a695e9e5f60985f8c649f
touhidulislamkhan <touhidikhan0@gmail.com>
2021-08-17 03:32:50 +0600

    secret key

--- helpers/environments.js
+++ helpers/environments.js
@@ -4,32 +4,35 @@
 const environments = {};
 
 environments.development = {
-  port: 3000,
-  envName: "development",
+   port: 3000,
+   envName: "development",
+   secretKey: "qwertyuiop",
 };
 
 environments.staging = {
-  port: 4000,
-  envName: "staging",
+   port: 4000,
+   envName: "staging",
+   secretKey: "asdfghjkl",
 };
 
 environments.production = {
-  port: 5000,
-  envName: "production",
+   port: 5000,
+   envName: "production",
+   secretKey: "zxcvbnm",
 };
 
 // choosing the environment
 
 const curerntEnvironment =
-  typeof process.env.NODE_ENV === "string"
-    ? process.env.NODE_ENV
-    : "development";
+   typeof process.env.NODE_ENV === "string"
+      ? process.env.NODE_ENV
+      : "development";
 
 //export corresponding environment
 
 const environmentToExport =
-  typeof environments[curerntEnvironment] === "object"
-    ? environments[curerntEnvironment]
-    : environments.development;
+   typeof environments[curerntEnvironment] === "object"
+      ? environments[curerntEnvironment]
+      : environments.development;
 
 module.exports = environmentToExport;
