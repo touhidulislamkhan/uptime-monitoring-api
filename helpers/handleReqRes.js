2f80cc9bc13c33c2ef34437859e04f5f20aae707
touhidulislamkhan <touhidikhan0@gmail.com>
2021-08-16 22:43:46 +0600

    setting content type

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -3,7 +3,7 @@ const url = require("url");
 const { StringDecoder } = require("string_decoder");
 const routes = require("../routes");
 const {
-  notFoundHandler,
+   notFoundHandler,
 } = require("../handlers/routeHandlers/notFoundHandler");
 
 // module scaffolding
@@ -11,53 +11,55 @@ const handler = {};
 
 //handling request response
 handler.handleReqRes = (req, res) => {
-  //handling the request
-  //parsing url
-  const parsedUrl = url.parse(req.url, true);
-  // const parsedUrl = new URL(req.url); //new whatwg url api
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/|\/+$/g, "");
-  const method = req.method.toLowerCase();
-  const queryStringObject = parsedUrl.query;
-  const headersObject = req.headers;
+   //handling the request
+   //parsing url
+   const parsedUrl = url.parse(req.url, true);
+   // const parsedUrl = new URL(req.url); //new whatwg url api
+   const path = parsedUrl.pathname;
+   const trimmedPath = path.replace(/^\/|\/+$/g, "");
+   const method = req.method.toLowerCase();
+   const queryStringObject = parsedUrl.query;
+   const headersObject = req.headers;
 
-  const requestProperties = {
-    parsedUrl,
-    path,
-    trimmedPath,
-    method,
-    queryStringObject,
-    headersObject,
-  };
+   const requestProperties = {
+      parsedUrl,
+      path,
+      trimmedPath,
+      method,
+      queryStringObject,
+      headersObject,
+   };
 
-  const decoder = new StringDecoder("utf-8");
-  let realData = "";
+   const decoder = new StringDecoder("utf-8");
+   let realData = "";
 
-  const chosenHandler = routes[trimmedPath]
-    ? routes[trimmedPath]
-    : notFoundHandler;
+   const chosenHandler = routes[trimmedPath]
+      ? routes[trimmedPath]
+      : notFoundHandler;
 
-  req.on("data", (buffer) => {
-    realData += decoder.write(buffer);
-  });
+   req.on("data", (buffer) => {
+      realData += decoder.write(buffer);
+   });
 
-  req.on("end", () => {
-    realData += decoder.end();
+   req.on("end", () => {
+      realData += decoder.end();
 
-    chosenHandler(requestProperties, (statusCode, payload) => {
-      statusCode = typeof statusCode === "number" ? statusCode : 500;
-      payload = typeof payload === "object" ? payload : {};
+      chosenHandler(requestProperties, (statusCode, payload) => {
+         statusCode = typeof statusCode === "number" ? statusCode : 500;
+         payload = typeof payload === "object" ? payload : {};
 
-      const payloadString = JSON.stringify(payload);
+         const payloadString = JSON.stringify(payload);
 
-      res.writeHead(statusCode);
-      res.end(payloadString);
-    });
-    //handle response
-    res.end("hello world");
-  });
+         res.setHeader("Content-Type", "application/json");
+         res.writeHead(statusCode);
+         res.end(payloadString);
+      });
+      //handle response
+      res.end("hello world");
+   });
+   //  console.log(res);
 
-  // console.log(parsedUrl);
+   // console.log(parsedUrl);
 };
 
 module.exports = handler;
