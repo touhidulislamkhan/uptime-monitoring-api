5c538e2ca65ad8a1d4d2b8f88b87c9529b1f8b71
touhidulislamkhan <touhidikhan0@gmail.com>
2021-08-17 19:43:10 +0600

    token route

--- routes.js
+++ routes.js
@@ -1,11 +1,13 @@
 //dependencies
 const { sampleHandler } = require("./handlers/routeHandlers/sampleHandler");
+const { tokenHandler } = require("./handlers/routeHandlers/tokenHandler");
 const { userHandler } = require("./handlers/routeHandlers/userHandler");
 
 //module scaffolding
 const routes = {
    sample: sampleHandler,
    user: userHandler,
+   token: tokenHandler,
 };
 
 module.exports = routes;
